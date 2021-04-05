6452f56bc634ccf74babe14adbea93870edc0bfc
VikKalya <79197340+VikKalya@users.noreply.github.com>
2021-04-05 13:44:45 +0300

    Add files via upload

---
+++ spring-helloworld/src/main/java/App.java
@@ -0,0 +1,19 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld world1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld world2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 =
+                (Cat) applicationContext.getBean("cat");
+        Cat cat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(world1 == world2);
+        System.out.println(cat1 == cat2);
+    }
+}
